e24e291edf2d437155c7bfdb84a1ad91ae014983
Perusina <petzydrummer@gmail.com>
2019-07-04 16:48:14 -0600

    added redirections to controller

--- add_member_controller.php
+++ add_member_controller.php
@@ -20,31 +20,18 @@ if ( isset($_POST["sub"]) && isset($_POST["carne"]) && isset($_POST["name"])) {
         die("Connection failed: " . $conn->connect_error);
     } 
 
-    $sql = "SELECT name, carne FROM class_group where name = ".$_POST['name']. " and carne = ".$_POST['carne'];
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
+    $sql = "INSERT INTO class_group (name, carne) values(".$_POST['name']. ", ".$_POST['carne']. ")";
+    if ($conn->query($sql) === TRUE) {
+        $_SESSION['errorMessage'] = "Miembro ingresado exitosamente.";
         header ("Location: members.php");
     } else {
-        $errorMsg = "Credenciales erróneas";
+        $_SESSION['errorMessage'] = "Ha ocurrido un error interno.";
+        header ("Location: members.php");
     }
     $conn->close();
 } else {
     $_SESSION['errorMessage'] = "Por favor ingrese nombre y carné";
+    header ("Location: members.php");
 }
 
-
-$servername = "database-server";
-$username = "root";
-$password = "";
-$dbname = "redes2grupo4";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "SELECT name, carne FROM class_group";
-$result = $conn->query($sql);
 ?>
